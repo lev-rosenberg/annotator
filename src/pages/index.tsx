178ea569514d01df95289ca85ac2cd989ef992bb
Lev Rosenberg <lev.rosenberg@gmail.com>
2023-09-03 13:14:20 -0400

    delete minidemos

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,4 @@
 import React from "react";
-import SvgAnnotator from "../components/mini-demos/svgAnnotator";
-import PaperAnnotator from "../components/mini-demos/paperAnnotator";
-import SVGJSAnnotator from "../components/mini-demos/svgjsAnnotator";
-import TwoAnnotator from "../components/mini-demos/twoAnnotator";
 import KonvaViewer from "./KonvaViewer";
 
 import Link from "next/link";
